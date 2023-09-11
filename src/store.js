613d5cbd32d6b6a7c9d49abccadaf3c2e3e400b4
Eslam Genedy <eslamgenedy377@yahoo.com>
2023-09-11 22:27:29 +0300

    creating project slice

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 // store.js
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import projectReducer from "./slices/projectSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    projects: projectReducer,
   },
 });
 
